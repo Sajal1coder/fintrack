db2fd99e28e587ad89ed17c1961e58b2a25b46ed
Sajal1coder <157198364+Sajal1coder@users.noreply.github.com>
2025-02-24 12:02:06 +0530

    Header Added

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -4,10 +4,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/"/>
-      <Button variant={"destructive"}>
-        click me
-      </Button>
+      
     </div>
   );
 }
